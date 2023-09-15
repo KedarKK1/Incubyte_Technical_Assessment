3eff1053fe4ab5716538fe93fbbdf8581603bb6b
KedarKK1 <officialkedark1@gmail.com>
2023-09-15 07:54:58 +0530

    :triangular_flag_on_post: getters & setters working, tested using user-input
    next - add methods to move up,down,right,left,forward, backward & set user-input accordingly

--- chandrayaan3_control.py
+++ chandrayaan3_control.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Chandrayaan3:
     def __init__(self):
         self.position = (0, 0, 0)
@@ -17,3 +20,39 @@ class Chandrayaan3:
         self.direction = dir
     # getters and setters completed
 
+    def takeUserInput(self):
+
+        # initializing variable
+        ch = ''
+
+        # while user-input is not 'c' or 'C', take user input
+        while ch != "c" and ch != "C":
+            print("Enter input (g - get position, d - get direction, x - set x position, y - set y position) or end input (c/C): ")
+            ch = str(input())
+
+            if(ch == "g"):
+                print(self.get_position())
+            elif(ch == "d"):
+                print(self.get_direction())
+            elif(ch == "x"):
+                print("Enter Position corordinate X Y Z: ")
+                x, y, z = map(int, sys.stdin.readline().split())
+                print(self.set_position(x, y, z))
+                print("Position is set to", self.get_position())
+            elif(ch == "y"):
+                print("Enter Directions(E/W/S/N): ")
+                k = str(input())
+                if(k == "E" or k == "W" or k == "S" or k == "N"):
+                    print(self.set_direction(k))
+                    print("Direction is set to", self.get_direction())
+                else:
+                    print("Invalid directions! ")
+            elif(ch == "c" or ch == "C"):
+                continue
+            else:
+                print("Error! Enter a valid input(f/b/l/r/u/d/c/C)")
+
+
+# Creating obj for Chandrayaan3, for calling takeUserInput function, for taking user input
+obj = Chandrayaan3()
+obj.takeUserInput()
